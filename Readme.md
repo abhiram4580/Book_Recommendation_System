1c6885e3cb7538be07fcfeab499d251b8ea39f8d
abhiram4580 <yalla.21ug1054@iiitranchi.ac.in>
2024-01-30 13:07:06 +0530

    committed

---
+++ Readme.md
@@ -0,0 +1,60 @@
+
+# Book Recommendation System
+
+## Overview
+This project implements a simple book recommendation system using Streamlit, a Python library for creating interactive web applications. The recommendation system is based on collaborative filtering and popularity based recommendation system provides personalized book suggestions to users.
+
+The Project has two main objectives:
+
+### Objective 1
+To display Top 20 most suggested books or popular books which are filtered through from the data provided using Popularity Based Recommendation System.
+
+### Objective 2
+To recommend 5 books to user enter book.This is done by using collaborative based Recommendation System which uses cosine similiarity search.
+And in this project the major concern was to focus on user ratings and make recommendation based on that.
+
+Streamlit framework is being used for its deployment.
+
+## Dependencies
+  -Python 3.x
+  -Streamlit
+  -Pandas
+  -Scikit-learn (for collaborative filtering)
+  -pickle 
+  
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/abhiram4580/Book_Recommendation_System.git
+    
+
+2.Install the Dependencies by
+    ```bash
+    pip install -r requirements.txt
+
+
+## Data
+Download from: https://www.kaggle.com/datasets/arashnic/book-recommendation-dataset
+
+There is 3 csv files available for the project.
+    -Books.csv
+    -Ratings.csv
+    -Users.csv
+After downloading 3 of them, you can start cleaning and building a model.
+In this project, Cosine Similarity was used for building collaborative filtering recommender system.
+
+## Usage
+    1. Run the Streamlit app by:
+    ```bash
+    streamlit run app.py
+    2.Open your web browser and navigate to http://localhost:8501 to access the book recommendation system.
+    3.Input your preferences or choose a user profile to get personalized book recommendations.
+    
+
+
+   
+  
+
+
--- app.py
+++ app.py
@@ -4,99 +4,175 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pickle
 import requests
-
-books_dict= pickle.load(open('popular.pkl','rb'))
-books=pd.DataFrame(books_dict)
-similar= pickle.load(open('similarity_score.pkl','rb'))
-
-def fetch_poster(books_id):
-    url= 'https://covers.openlibrary.org/b/id/'+str(books_id)+'-M.jpg' # url for the book covers
-    try:
-        data=requests.get(url) # getting the data from the url
-        data.raise_for_status() # checking  for the status 
-        data=data.json() # converting the data into json format
-        poster_path=data['poster_path'] # getting the poster path
-        def fetch_poster(books_id):
-            url = 'https://covers.openlibrary.org/b/id/' + str(books_id) + '-M.jpg'  # url for the book covers
-            try:
-                data = requests.get(url)  # getting the data from the url
-                data.raise_for_status()  # checking for the status
-                data = data.json()  # converting the data.json format
-                poster_path = data['poster_path']# getting the poster path
-                full_path = 'https://covers.openlibrary.org' + poster_path  # get the full path for the book
-                return full_path
-            except requests.exceptions.HTTPError as err:
-                print(f"HTTP Error: {err}")
-            except requests.exceptions.RequestException as err:
-                print(f"Request Exception: {err}")
-
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP Error: {err}")
-
-def recommend(book):
-    try:
-      index=books[books['title']==book].index[0]
-      distances=similar[index]# get the distances of the book
-      books_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:11] # getting the top 10 books.
-      recommended_books=[]
-      recommended_books_posters=[]
-      for i in books_list:
-          books_id=books.iloc[i][0].split('-')[1] # getting the books id
-          recommended_books.append(books.iloc[i][0]) # getting the name of the book.
-          poster_path=fetch_poster(books_id)
-          if poster_path:
-              recommended_books_posters.append(poster_path)
-
-          return recommended_books,recommended_books_posters
-      
-    except:
-         st.write("Sorry,the book which you are looking for is not present")
+import json 
 
 
-# Streamlit Layout 
-st.title("Book Recommender System") 
-option=st.selectbox("Select a book",books['Book-Title'].values) #
-
-if st.button('Recommend'):
-    if(option is not None):
-        names,posters=recommend(option)
+def recommend(book_name):
+    if book_name not in pivot.index: # Checking if the book is in the list
+        return None
     else:
-        st.write("Please select a book")
+        book_id=pivot.index.get_loc(book_name) # Getting the index of the book
+        similar_books=list(enumerate(similar[book_id])) # Getting the list of books with their similarity score
+        similar_books=sorted(similar_books,key=lambda x:x[1],reverse=True) # Sorting the book_list in descending order of similarity score
+        similar_books=similar_books[1:6] # Selecting the top 5 books
+        data=[]
+        for i in similar_books:
+            item=[]
+            df=books[books['Book-Title']==pivot.index[i[0]]]
+            item.extend(list(df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+            data.append(item)
+        return data    
+st.title("Book Recommender System")
 
-# Displaying the selected book information
-    selected_book=books[books['Book-Title']==option].iloc[0]
-    st.write(f"**Selected Book: {selected_book['Book-Title']} **" )
-    column1,column2,column3,column4,column5,column6,column7,column8,column9,column10=st.columns(10)
-    with column1:
-        st.text(names[0])
-        st.image(posters[0],use_column_width=True)
-    with column2:
-        st.text(names[1])
-        st.image(posters[1],use_column_width=True)
-    with column3:
-        st.text(names[2])
-        st.image(posters[2],use_column_width=True)
-    with column4:
-        st.text(names[3])
-        st.image(posters[3],use_column_width=True)
-    with column5:
-        st.text(names[4])
-        st.image(posters[4],use_column_width=True)
-    with column6:
-        st.text(names[5])
-        st.image(posters[5],use_column_width=True)
-    with column7:
-        st.text(names[6])
-        st.image(posters[6],use_column_width=True)
-    with column8:
-        st.text(names[7])
-        st.image(posters[7],use_column_width=True)
-    with column9:
-        st.text(names[8])
-        st.image(posters[8],use_column_width=True)
-    with column10:
-        st.text(names[9])
-        st.image(posters[9],use_column_width=True)
-    
-    
-            
+popular=pickle.load(open('popular.pkl','rb'))
+books=pickle.load(open('books.pkl','rb'))
+books=pd.DataFrame(books)
+similar=pickle.load(open('similarity_score.pkl','rb'))
+pivot=pickle.load(open('pivot_t.pkl','rb'))
+
+
+
+book_list=pivot.index.values
+image_url=popular["Image-URL-M"].tolist()
+book_title=popular['Book-Title'].tolist()
+book_author=popular['Book-Author'].tolist()
+total_ratings=popular['Number-of-Ratings'].tolist()
+avg_ratings=popular['Average-Ratings'].tolist()
+
+st.sidebar.title("Top 20 Books")
+if st.sidebar.button("SHOW"):
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.image(image_url[0])
+        st.text(book_author[0])
+        st.text("Ratings:" + str(total_ratings[0]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[0],2)))
+    with col2:
+        st.image(image_url[1])
+        st.text(book_author[1])
+        st.text("Ratings:" + str(total_ratings[1]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[1],2)))
+    with col3:
+        st.image(image_url[2])
+        st.text(book_author[2])
+        st.text("Ratings:" + str(total_ratings[2]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[2],2)))
+    with col4:
+        st.image(image_url[3])
+        st.text(book_author[3])
+        st.text("Ratings:" + str(total_ratings[3]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[3],2)))
+    with col5:
+        st.image(image_url[4])
+        st.text(book_author[4])
+        st.text("Ratings:" + str(total_ratings[4]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[4],2)))
+        
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.image(image_url[5])
+        st.text(book_author[5])
+        st.text("Ratings:" + str(total_ratings[5]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[5],2)))
+    with col2:
+        st.image(image_url[6])
+        st.text(book_author[6])
+        st.text("Ratings:" + str(total_ratings[6]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[6],2)))
+    with col3:
+        st.image(image_url[7])
+        st.text(book_author[7])
+        st.text("Ratings:" + str(total_ratings[7]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[7],2)))
+    with col4:
+        st.image(image_url[8])
+        st.text(book_author[8])
+        st.text("Ratings:" + str(total_ratings[8]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[8],2)))
+    with col5:
+        st.image(image_url[9])
+        st.text(book_author[9])
+        st.text("Ratings:" + str(total_ratings[9]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[9],2)))
+
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.image(image_url[10])
+        st.text(book_author[10])
+        st.text("Ratings:" + str(total_ratings[10]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[10],2)))
+    with col2:
+        st.image(image_url[11])
+        st.text(book_author[11])
+        st.text("Ratings:" + str(total_ratings[11]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[11],2)))
+    with col3:
+        st.image(image_url[12])
+        st.text(book_author[12])
+        st.text("Ratings:" + str(total_ratings[12]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[12],2)))
+    with col4:
+        st.image(image_url[13])
+        st.text(book_author[13])
+        st.text("Ratings:" + str(total_ratings[13]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[13],2)))
+    with col5:
+        st.image(image_url[14])
+        st.text(book_author[14])
+        st.text("Ratings:" + str(total_ratings[14]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[14],2)))  
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.image(image_url[15])
+        st.text(book_author[15])
+        st.text("Ratings:" + str(total_ratings[15]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[15],2)))
+    with col2:
+        st.image(image_url[16])
+        st.text(book_author[16])
+        st.text("Ratings:" + str(total_ratings[16]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[16],2)))
+    with col3:
+        st.image(image_url[17])
+        st.text(book_author[17])
+        st.text("Ratings:" + str(total_ratings[17]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[17],2)))
+    with col4:
+        st.image(image_url[18])
+        st.text(book_author[18])
+        st.text("Ratings:" + str(total_ratings[18]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[18],2)))
+    with col5:
+        st.image(image_url[19])
+        st.text(book_author[19])
+        st.text("Ratings:" + str(total_ratings[19]))
+        st.text("Avg.Rating:" + str(round(avg_ratings[19],2))) 
+
+st.sidebar.title("Recommend Books")
+selected_book = st.sidebar.selectbox("Type or select a book from the dropdown", book_list)
+
+if st.sidebar.button("Recommend Me"):
+    moviee = recommend(selected_book)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.image(moviee[0][2])
+        st.text(moviee[0][0])
+        st.text(moviee[0][1])  
+    with col2:
+        st.image(moviee[1][2])
+        st.text(moviee[1][0])
+        st.text(moviee[1][1])
+    with col3:
+        st.image(moviee[2][2])
+        st.text(moviee[2][0])
+        st.text(moviee[2][1])
+    with col4:
+        st.image(moviee[3][2])
+        st.text(moviee[3][0])
+        st.text(moviee[3][1])
+    with col5:
+        st.image(moviee[4][2])
+        st.text(moviee[4][0])
+        st.text(moviee[4][1])
